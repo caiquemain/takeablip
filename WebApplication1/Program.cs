3a09a69533e48b3c67995a5802c88a799ac4f07d
Caique Main <caiquemain@gmail.com>
2024-12-20 00:31:21 -0300

    adcionado redirecionamento

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -16,10 +16,13 @@ builder.Services.AddSwaggerGen(options =>
 // Configuração explícita da porta exigida pelo Render
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(8080); // Porta padrão do Render1
+    options.ListenAnyIP(8080); // Porta padrão do Render
 });
 
 var app = builder.Build();
+
+// Redirecionar a raiz para o GitHub
+app.UseRewriter(new RewriteOptions().AddRedirect("^$", "https://github.com/caiquemain/takeablip"));
 
 // Configurar middleware de pipeline HTTP
 if (app.Environment.IsDevelopment())
@@ -29,7 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "GitHub Repo Lister API v1");
-        options.RoutePrefix = string.Empty; // Swagger na raiz
+        options.RoutePrefix = "swagger"; // Swagger estará em /swagger
     });
 }
 
